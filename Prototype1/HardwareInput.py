623738bc75e8625a0e2993d72612c45e25059762
Your NameTomRiej <Tom8434@dubaicollege.org>
2021-08-14 21:28:01 +0400

    Added threading for reading

--- Prototype1/HardwareInput.py
+++ Prototype1/HardwareInput.py
@@ -1,3 +1,79 @@
   # Requirements
 # getInputSuccess()
 #
+import serial
+from time import sleep, time
+from threading import Thread
+
+
+class HallInput:
+    def __init__(self, distBetweenMagnets: float) -> None:
+        self.__ser = serial.Serial("/dev/tty.usbmodem14101", 9600, timeout=.5)
+        sleep(2) # allow time for the serial object to connect to port
+        
+        self.__DIST_BETWEEN_MAGNETS = distBetweenMagnets
+        self.__ENCODING = "utf-8"
+        self.__EMPTY = ""
+     
+        self.__speed = 0.0 
+        self.__displacement = 0
+        
+        self.__sensorsPassed = {}
+        self.__continueReading = False
+
+
+    def __calcSpeed(self, timeMillis: int) -> float:
+        if timeMillis == 0:
+            print("Time value is 0 causing division by 0 error")
+            return 0.0
+        else:
+            timeSec = timeMillis / 1000
+            return self.__DIST_BETWEEN_MAGNETS / timeSec
+        
+    def __readSerialPort(self) -> None:
+        while self.__continueReading:
+            line = self.__ser.readline()
+            strLine = line.decode(self.__ENCODING)
+            if strLine != self.__EMPTY:
+                disp, timeTaken = strLine.split(" ")
+                self.__displacement = int(disp)
+                self.__speed = self.__calcSpeed(float(timeTaken))
+                self.__sensorsPassed[self.__displacement] = time()
+            
+    def startReading(self):
+        # only start reading if it isn't already started
+        if not self.__continueReading:
+            # creating a thread for reading in, so i dont disturb any loops that use this module
+            self.__thread = Thread(target=self.__readSerialPort)
+            self.__continueReading = True
+            self.__thread.start()
+        else:
+            print("reading has already started")
+        
+    def stopReading(self):
+        self.__continueReading = False
+        self.__thread.join()
+            
+    def getSensorsPassed(self):
+        return self.__sensorsPassed
+            
+    def getSpeed(self) -> float:
+        return self.__speed
+    
+    def getDisplacement(self) -> int:
+        return self.__displacement
+    
+    def closeSerial(self) -> None:
+        self.__ser.close()
+        
+        
+if __name__ == '__main__':
+      
+    # 10cm = 0.1 m           
+    hallReader = HallInput(0.1)
+
+    for i in range(20):
+        hallReader.readSerialPort()
+        print(f"SensorNumber: {hallReader.getDisplacement()} Speed: {hallReader.getSpeed()}")
+    print(hallReader.getSensorsPassed())
+        
--- Prototype1/HardwareInputOutputControl.py
+++ Prototype1/HardwareInputOutputControl.py
@@ -2,3 +2,64 @@
 # 2 threads: one for input, one for output
 # getInputSuccess()
 # getRecentCarInfo()
+from threading import Thread
+
+
+from HardwareInput import *
+from HardwareOutput import *
+
+
+# class needed so that I am able to stop the thread at will using
+# class attributs with sufficient scope
+# class readerThread:
+#     def __init__(self, reader):
+#         self.__hallReader = reader
+#         self.__thread = Thread(target=self.constantRead)
+#         self.__continue = True
+        
+        
+#     def constantRead(self):
+#         while self.__continue:
+#             self.__hallReader.readSerialPort()
+#             print(f"SensorNumber: {self.__hallReader.getDisplacement()} Speed: {self.__hallReader.getSpeed()}")
+
+#     def start(self):
+#         self.__thread.start()
+            
+#     def stop(self):
+#         self.__continue = False
+#         self.__thread.join()
+#         print("finish")
+        
+        
+        
+class HardwareController:
+    def __init__(self, distanceBetweenMagnets):
+        self.__hallEffectSensorInput = HallInput(distanceBetweenMagnets)
+        self.__servoController = None
+    
+    def getNumHallInputs(self):
+        return len(self.__hallEffectSensorInput.getSensorsPassed())
+    
+    def startReading(self):
+        self.__hallEffectSensorInput.startReading()
+        
+    def stopReading(self):
+        self.__hallEffectSensorInput.stopReading()
+        
+    def close(self):
+        self.__hallEffectSensorInput.closeSerial()
+        
+    
+if __name__=="__main__":
+    from time import sleep
+    h = HardwareController(0.1)
+    for i in range(2):
+        print("starting")
+        h.startReading()
+        for i in range(10):
+            print(h.getNumHallInputs())
+            sleep(1)
+        h.stopReading()
+        print("finished")
+        sleep(2)
